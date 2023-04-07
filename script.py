9c78985fd3832c0dbc0286fea37de85ad524ce00
makovka44 <maj.mohar4@gmail.com>
2023-04-07 21:51:06 +0200

    cajt dela

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 import discord
 import asyncio
-import datetime
+
+from datetime import datetime
+
+datetime.today()
 import os
-import dotenv
 
 from dotenv import load_dotenv
 load_dotenv()
 
-stop = 1
+stop = 0
 
 # Replace YOUR_TOKEN with your own Discord bot token
 TOKEN = os.getenv("moj_token")
@@ -27,10 +29,9 @@ async def my_background_task():
     global stop
     await client.wait_until_ready()
 
-    while not client.is_closed():
-        if stop == 0:
-            await send_dm("Hello!!!!")
-            await asyncio.sleep(1)
+    if stop == 0:
+        await send_dm("Hello!!!!")
+        await asyncio.sleep(1)
 
 
 async def send_dm(message):
@@ -39,15 +40,17 @@ async def send_dm(message):
 
 
 async def send_spam(user):
+    print("trying to spam")
     message=""
     #Naredi neko spotročilo, ki ga potem pošlje
 
     for i in range(1, 10):
-        stvar_za_prilepit=f"<@{user}\n>"
+        stvar_za_prilepit=f"<@{USER_ID}/n>"
         message+=stvar_za_prilepit
     print(message)
     user = await client.fetch_user(user)
     await user.send(message)
+    print("sent spam")
    
 
 
@@ -57,7 +60,8 @@ async def on_ready():
     client.loop.create_task(my_background_task())
     # Send a DM right away when the bot is ready
     await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="Slova je zakon!"))
-    await asyncio.sleep(5)
+    await asyncio.sleep(1)
+    print("status urejen")
 
 
 @client.event
@@ -71,31 +75,46 @@ async def on_message(message):
     # Respond with a custom message if a specific phrase is mentioned in the message
     if 'maj' in message.content.lower():
         await message.channel.send(f'Govorim o {message.author.mention}!')
+        print("omenil sem maja")
+
+    if '/meme' in message.content.lower():
+        await message.channel.send(f'Govorim o {message.author.mention}!')
+        print("omenil sem maja")
 
     elif 'stop' in message.content.lower():
         stop = 1
         await message.channel.send(f'končal sem!')
+        print("končal sem")
 
     elif 'nadaljuj' in message.content.lower():
         await message.channel.send(f'začenjam')
+        print("začenjam")
         stop = 0
 
     elif "pojdi k juriju" in message.content.lower():
         await send_dm("**Adijos!**")
         USER_ID = JURIJ
+        print("jurij")
     elif "pojdi k jerneju" in message.content.lower():
         await send_dm("**Adijos!**")
         USER_ID = JERNEJ
+        print("jernej")
     elif "pojdi k matiju" in message.content.lower():
         await send_dm("**Adijos!**")
         USER_ID = MATIJA
+        print("matija")
     elif "pridi nazaj" in message.content.lower():
-        await send_dm("**Hello!**")
         USER_ID = MAJ
+        await send_dm("**Hello!**")
+        print("maj")
     elif "pojdi k linu" in message.content.lower():
         await send_dm("**Adijos!**")
         USER_ID = LIN
-    elif "cas" in message.content.lower():
-        await send_dm(datetime.datetime.now())
-        print(datetime.datetime.now())
+        print("lin")
+    elif "čas" in message.content.lower():  
+        await send_dm(f"Čas je {datetime.today()}")
+        print(datetime.today())
+    elif "spam" in message.content.lower():
+        await send_spam(USER_ID)
+        print("sent spam")
 client.run(TOKEN)
