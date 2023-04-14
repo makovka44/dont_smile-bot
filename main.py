bc2a00333237f23405b93ed7632cc3b51a4f9a33
makovka44 <maj.mohar4@gmail.com>
2023-04-14 22:36:05 +0200

    finish vsega + za host

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import discord
 import asyncio
 import aiohttp
 import random
+import io
 
 from datetime import datetime
+from discord.utils import get
 
 
 datetime.today()
@@ -24,12 +26,8 @@ TOKEN = os.getenv("moj_token")
 USER_ID = os.getenv("USER_ID")
 JURIJ = os.getenv("JURIJ")
 MAJ = os.getenv("MAJ")
-JERNEJ = os.getenv("JERNEJ")
-MATIJA = os.getenv("MATIJA")
 LIN = os.getenv("LIN")
 VAL = os.getenv("VAL")
-JAKA = os.getenv("JAKA")
-URBAN = os.getenv("URBAN")
 
 async def send_channel(channel, message):
     channel = client.get_channel(channel)
@@ -38,6 +36,7 @@ async def send_channel(channel, message):
 async def my_background_task():
     global stop
     await client.wait_until_ready()
+    await send_dm("Online sem")
     await asyncio.sleep(1)
 
 
@@ -49,12 +48,8 @@ async def send_dm2(user, message):
     user = await client.fetch_user(user)
     await user.send(message)
 
-async def get_sender_id(message):
-    sender_id = message.author.id
-    print(sender_id)
-    return sender_id
-
 async def send_spam(user, channel):
+    print("trying to spam")
     message=""
     #Naredi neko spotročilo, ki ga potem pošlje
 
@@ -64,7 +59,6 @@ async def send_spam(user, channel):
     print(message)
     user = await client.fetch_user(user)
     await channel.send(message)
-    await send_dm2(MAJ, "sent spam")
    
 async def get_channel_id(message):
     channel_id = message.channel.id
@@ -73,21 +67,14 @@ async def get_channel_id(message):
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    await send_dm2(MAJ, 'Logged in as {0.user}'.format(client))
     client.loop.create_task(my_background_task())
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="Slova je zakon!"))
-    #await asyncio.sleep(1)
-    await send_dm2(MAJ, "status urejen")
-    await send_dm2(MAJ, "Hello!!!!")
-    await send_dm2(MAJ, "hostan pr juriju")
-
-@client.event
-async def send_channel(channel, message):
-    channel = message.get_channel(channel)
-    await channel.send(message)
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="/help-me and Slova je zakon!"))
+    await asyncio.sleep(1)
+    await send_dm2(MAJ, "status urejen, sem pri juriju")
 
 async def send_meme(channel, message):
-    reddit_api = ["https://www.reddit.com/r/memes/hot.json?sort=hot", "https://www.reddit.com/r/terriblefacebookmemes/new.json?sort=hot", "https://www.reddit.com/r/ProgrammerHumor/hot.json?sort=hot", "https://www.reddit.com/r/HistoryMemes/hot.json?sort=hot", "https://www.reddit.com/r/traaaaaaannnnnnnnnns/hot.json?sort=hot", "https://www.reddit.com/r/Funnymemes/hot.json?sort=hot", "https://www.reddit.com/r/MinecraftMemes/hot.json?sort=hot", "https://www.reddit.com/r/AnarchyChess/hot.json?sort=hot", "https://www.reddit.com/r/PrequelMemes/new.json?sort=hot", "https://www.reddit.com/r/dadjokes/new.json?sort=hot"]
+    reddit_api = ["https://www.reddit.com/r/memes/hot.json?sort=hot", "https://www.reddit.com/r/terriblefacebookmemes/new.json?sort=hot", "https://www.reddit.com/r/ProgrammerHumor/hot.json?sort=hot", "https://www.reddit.com/r/HistoryMemes/hot.json?sort=hot", "https://www.reddit.com/r/traaaaaaannnnnnnnnns/hot.json?sort=hot", "https://www.reddit.com/r/Funnymemes/hot.json?sort=hot", "https://www.reddit.com/r/MinecraftMemes/hot.json?sort=hot", "https://www.reddit.com/r/AnarchyChess/hot.json?sort=hot", "https://www.reddit.com/r/PrequelMemes/new.json?sort=hot", "https://www.reddit.com/r/dadjokes/new.json?sort=hot", "https://www.reddit.com/r/Jokes/new.json?sort=hot"]
     izbira = random.choice(reddit_api)
     if izbira =="https://www.reddit.com/r/memes/hot.json?sort=hot":
         ime_meme = "classic meme"
@@ -109,115 +96,173 @@ async def send_meme(channel, message):
         ime_meme = "random meme"
     elif izbira == "https://www.reddit.com/r/dadjokes/new.json?sort=hot":
         ime_meme = "dad joke"
+    elif izbira == "https://www.reddit.com/r/Jokes/new.json?sort=hot":
+        ime_meme = "joke"
     description = (f"<@{message.author.id}> tukaj je tvoj "+ ime_meme+ ".")
-    embed = discord.Embed(title="Meme", description=description)
-
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get(izbira) as r:
-            res = await r.json()
-            data = res['data']['children'][random.randint(0, 25)]['data']
-            if data['post_hint'] == 'image':
-                embed.set_image(url=data['url'])
-            elif data['post_hint'] == 'hosted:video':
-                embed.set_image(url=data['thumbnail'])
-                embed.set_video(url=data['media']['reddit_video']['fallback_url'])
-                embed.video.height = data['media']['reddit_video']['height']
-                embed.video.width = data['media']['reddit_video']['width']
-            else:
-                # handle other post hints, such as link or rich:video
-                pass
-            await channel.send(embed=embed)
+    if ("dadjokes" or "jokes") in izbira:
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(izbira) as r:
+                res = await r.json()
+                post = random.choice(res['data']['children'])['data']
+                joke = post['title'] + '\n\n' + post['selftext']
+                embed = discord.Embed(title="Dad Joke", description=description, color=0xffa500)
+                embed= embed.add_field(name="Meme", value=joke, inline=False)
+                embed = embed.add_field(name="", value=joke, inline=False)
+                await channel.send(embed=embed)
+    else: 
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(izbira) as r:
+                res = await r.json()
+                children = res['data']['children']
+                post = random.choice(children)['data']
+                joke = "_" + post['title'] + '\n\n' + post['selftext'] + "_"
+                embed = discord.Embed(title="Meme", description=description, color=0xffa500)
+                embed= embed.add_field(name="", value=joke, inline=False)
+                if post['is_video']:
+                    video_url = post['media']['reddit_video']['fallback_url']
+                    async with cs.get(video_url) as r:
+                        video_bytes = await r.read()
+                    video_file = discord.File(io.BytesIO(video_bytes), filename='meme.mp4')
+                    await channel.send(file=video_file, embed=embed)
+                else:
+                    embed.set_image(url=post['url'])
+                    await channel.send(embed=embed)
 
 async def bot_sleep(time,message):
     await asyncio.sleep(1)
     async with message.channel.typing():
             await asyncio.sleep(time)
 
-async def send_8ball(channel):
-    seznam_odzivov = ["Eroor 404", "It is certain.", "It is decidely so.", "Without a doubt.", "Yes, definetly.", "You may rely on it", "A I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Sign point yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtfull."]
-    message = random.choice(seznam_odzivov)
-    await send_channel(channel, message)
-
 @client.event
 async def on_message(message):
-    lin=message.channel
+    #print(message)
+    lin=message.content
     cajt=message.channel
-    global stop, USER_ID, MAJ
+    global stop, USER_ID
     channel = message.channel.id
+    print(channel)
     channel = message.channel
-    #await client.http.delete_message(1015198933737291894, 1095783029047316530)
-    if message.author == client.user:
-        pass
-    
-    else:
-        await send_dm2(MAJ, message.content)
-    
-    if message.content.startswith("/stopaj_svojo_kodo"):
-        await send_dm2(MAJ, "Stopana koda.")
-        stop = 1
-    elif message.content.startswith("/nadaljuj_svojo_kodo"):
-        stop = 0
-        await send_dm2(MAJ, "nadaljujem kodo.")
-    elif stop == 1:
+
+    num_mentions = 0  # initialize the variable to 0
+
+    """if message.guild is not None:
+        # Split the last word of the message
+        žrtev = message.content.rsplit(None, 1)[-1]
+        # Get member object based on username
+        member = get(message.guild.members, name=žrtev)
+        if member is not None:
+            # Print member ID
+            print(member.id)
+        print(žrtev)
+        num_mentions = message.content.count("@")
+        print(num_mentions)
+
+    if num_mentions == 2:
+        # Timeout member for 10 seconds
+        await message.channel.set_permissions(member, send_messages=False)
+        await message.channel.send(f"<@{member}> has been timed out for 10 seconds, because of spamming.")
         await asyncio.sleep(10)
+        print("timeout")
+        await message.channel.set_permissions(member, send_messages=True)
+        await message.channel.send(f"<@{žrtev}> is no longer timed out.")
+    else:
+        pass"""
+
+
+
+    if message.author == client.user:
+        return
+    if (MAJ or VAL) == message.author:
+        if message.author.id == MAJ: mod = MAJ
+        elif message.author.id == VAL: mod = VAL
+        if message.content.startswith("/stopaj_svojo_kodo"):
+            await send_dm2(mod, "Stopana koda.")
+            stop = 1
+        if message.content.startswith("/nadaljuj_svojo_kodo"):
+            stop = 0
+            await send_dm2(mod, "nadaljujem kodo.")
+        elif stop == 1:
+            await asyncio.sleep(10)
     elif stop == 0 :
-        if message.content.lower() == "/help":
-            embed = discord.Embed(title='Command List', color=0xffa500)
+        if message.content.lower() == "/help-me":
+            embed = discord.Embed(title='Die Liste', color=0xffa500)
             embed.add_field(name='/meme-me', value='Generates a random meme', inline=False)
             embed.add_field(name='/spam-me', value='Sends multiple spam messages', inline=False)
-            embed.add_field(name='/ask-me', value='Answers a random question', inline=False)
+            embed.add_field(name='/ask-me', value='Answers a random question.', inline=False)
+            embed.add_field(name='/help-me', value='Shows this menu.', inline=False)
+            embed.add_field(name="/spam-c", value='Spams c=> custom.', inline=False)
             await message.channel.send(embed=embed)
-            await send_dm2(MAJ, "used /help")
-
-        elif 'maj' in message.content.lower():
-            await message.channel.send(f'Govorim o {message.author.mention}!')
-            bot_sleep(3, message)
-            await send_dm2(MAJ, "omenil sem maja")
+            await send_dm2(MAJ, "used help-me")
+        if ('maj'or"maju"or"majči") in message.content.lower():
+            await bot_sleep(1, message)
+            avtor = str(f"{message.author}")
+            avtor_send = str(avtor.split("#")[0])
+            await message.channel.send(f"{avtor_send} govori o <@{MAJ}>!")
+            send_dm2(MAJ, "omenil sem maja")
         elif "/meme-me" in message.content.lower():
             await bot_sleep(3,message)
-            await send_dm2(MAJ, "used meme")
             await message.delete()
+            await send_dm2(MAJ, "used meme.me")
             await send_meme(message.channel, message)
-        elif "/ask-me" in message.content.lower():
+        elif message.content.startswith("/ask-me: "):
+            lin = str(message.content.split(":")[1])
+            avtor = str(f"{message.author}")
+            avtor_send = "*_By "+ str(avtor.split("#")[0])+"_*"
             await bot_sleep(3, message)
             seznam_odzivov = ["Eroor 404", "It is certain.", "It is decidely so.", "Without a doubt.", "Yes, definetly.", "You may rely on it", "A I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Sign point yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtfull."]
-            message = random.choice(seznam_odzivov)
-            print("message izbran")
-            await send_dm2(MAJ, "8ball used")
-            await send_channel(channel, message)    
-        elif message.content.startswith("/spam"):
-                bot_sleep(3, message)
-                await send_dm2(MAJ, "/spam")
-                message=" "
-                uporabnik=message.content.split(" ")[:1]
-                stevilo=message.content.split(" ")[:2]
-                for i in range(0, stevilo): 
-                    sporočilce=message + (f"<@{uporabnik}>") + "\n"
-                await channel.send(message)
-        elif "/pojdi k jakatu" in message.content.lower():
-            bot_sleep(3, message)
-            await send_dm("**Adijos amigos!**")
-            USER_ID = JAKA
-            await send_dm("Hello my friend. I'm here to meme you. Use /meme-me to get memes.")
-        elif "/pojdi k maju" in message.content.lower():
-            await send_dm("**Adijos amigos!**")
-            USER_ID = MAJ
-            await send_dm("Hello my friend. I'm here to meme you. Use /meme-me to get memes.")
-        elif "/pojdi k urbanu" in message.content.lower():
-            await send_dm("**Adijos amigos!**")
-            USER_ID = URBAN
-        elif "/čas" in message.content.lower():  
-            bot_sleep(3, message)
-            mesič=(f"Čas je {datetime.today()}")
-            await send_channel(mesič, cajt)   
-            print(datetime.today())
+            message_to_send = random.choice(seznam_odzivov)
+            embed = discord.Embed(title=lin, color=0xffa500)
+            embed.add_field(name='Answer:', value= message_to_send, inline=False)
+            embed.add_field(name="", value= avtor_send, inline=False)
+            await message.channel.send(embed=embed) 
+            await send_dm2(MAJ, "used as-me")  
+            await message.delete()
+        elif "/čas" in message.content.lower():
+            await bot_sleep(3, message)
+            now = datetime.now()
+            mesic = now.strftime("%d.%m.%Y, %H:%M")
+            embed = discord.Embed(title="Čas", description=mesic, color=0xffa500)
+            await channel.send(embed=embed)
+            await send_dm2(MAJ, "uporabljen čas")
+            await message.delete()
         elif "/spam-me" in message.content.lower():
-            bot_sleep(3, message)
+            await bot_sleep(3, message)
             USER_ID = message.author.id
             await send_spam(USER_ID, cajt)
-            print("sent spam")
-        elif "/spamaj" in message.content.lower():
-            bot_sleep(3, message)
+            await message.delete()
+            send_dm2(MAJ, "sent spam")
+        elif message.content.startswith("/spam-c "):
+            await message.delete()
+            user = message.content.split(" ")[1]
+            await bot_sleep(3, message)
+            stevilka = int(message.content.split(" ")[2])
+            if stevilka * len(user)>3900:
+                stevilka=3900//len(user)
+            message_to_send = " "
+            for i in range(1, stevilka):
+                stvar_za_prilepit=f"<@{user}>"
+                message_to_send+=stvar_za_prilepit + "\n"
+            avtor = str(f"{message.author}")
+            message_to_send+= "By "+ str(avtor.split("#")[0])
+            await channel.send(message_to_send)
+            await send_dm2(MAJ, message_to_send)
+        elif message.content.startswith("/custom"):
+            if message.author.id == MAJ:    
+                channel_id = message.content.split(" ")[1]
+                message_id = message.content.spli(" ")[2]
+                await client.http.delete_message(channel_id, message_id)
+                await message.delete()
+                await send_dm2(MAJ, "deleted")
+        elif "/pošlji-manual" in message.content.lower():
+            await bot_sleep(3, message)
+            await message.delete()
             print("spamanje")
+            embed = discord.Embed(title='Die Liste', color=0xffa500)
+            embed.add_field(name='/meme-me', value='Generates a random meme.', inline=False)
+            embed.add_field(name='/spam-me', value='Sends multiple spam messages.', inline=False)
+            embed.add_field(name='/ask-me: ', value='Answers a random question.', inline=False)
+            embed.add_field(name='/help-me', value='Shows this menu.', inline=False)
+            embed.add_field(name="/spam-c", value='Spams c=> custom.', inline=False)
+            await message.channel.send(embed=embed)
 
 client.run(TOKEN)
